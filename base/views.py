5c68dc3aa61b55f20770a71facf5011ca834df14
hesam ramezani <hesamramezani212121@gmail.com>
2022-11-12 10:34:00 -0500

    create routes url and views

--- base/views.py
+++ base/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+routes = [
+    '/api/v1/products/',
+    '/api/v1/products/create',
+
+    '/api/v1/products/upload/',
+
+    '/api/v1/products/<id>/reviews/',
+
+    '/api/v1/products/top/',
+    '/api/v1/products/<id>/',
+
+    '/api/v1/products/delete/<id>/',
+    '/api/v1/products/<update>/<id>/',
+
+]
+
+@api_view(http_method_names=["GET"])
+def GetRoutes(request):
+    return Response(routes)
