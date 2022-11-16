c5f4ae32db09f45b0035e8ac93ae2f8c99251dcd
hesam ramezani <hesamramezani212121@gmail.com>
2022-11-16 12:58:48 -0500

    set images root

--- base/models.py
+++ base/models.py
@@ -6,15 +6,13 @@ class Product(models.Model):
     _id = models.AutoField(primary_key=True, editable=False)
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
     name = models.CharField(max_length=200, null=True)
-    # image =
+    image = models.ImageField(null=True , blank=True)
     brand = models.CharField(max_length=200, null=True, blank=True)
     category = models.CharField(max_length=200, null=True, blank=True)
     description = models.TextField(null=True, blank=True)
-    rating = models.DecimalField(
-        max_digits=3, decimal_places=2, null=True, blank=True)
+    rating = models.DecimalField(max_digits=3, decimal_places=2, null=True, blank=True)
     numReviews = models.IntegerField(null=True, blank=True, default=0)
-    price = models.DecimalField(
-        max_digits=9, decimal_places=2, null=True, blank=True)
+    price = models.DecimalField(max_digits=9, decimal_places=2, null=True, blank=True)
     countInStock = models.IntegerField(null=True, blank=True, default=0)
     createdAt = models.DateTimeField(auto_now_add=True)
 
